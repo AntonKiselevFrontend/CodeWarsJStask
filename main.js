b1efc55e395cda32227f4118b3a35c6cea36231f
Anton Kiselev <104986333+AntonKiselevFrontend@users.noreply.github.com>
2023-06-16 02:31:11 +0400

    Update main.js

--- main.js
+++ main.js
@@ -57,3 +57,9 @@ function updateLight(current) {
 function validatePIN (pin) {
   return /^(\d{4}|\d{6})$/.test(pin);
 }
+//Simple, given a string of words, return the length of the shortest word(s).
+//String will never be empty and you do not need to account for different data types.
+function findShort(s){
+  let arr = Math.min(...s.split(" ").map(item => item.length))
+  return arr
+}
