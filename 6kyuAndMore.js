7b056bf07161b72acfa8a681d16973a1d3badbf9
Anton Kiselev <104986333+AntonKiselevFrontend@users.noreply.github.com>
2023-06-18 17:47:19 +0400

    Update 6kyuAndMore.js

--- 6kyuAndMore.js
+++ 6kyuAndMore.js
@@ -29,3 +29,12 @@ function persistence(num) {
   }
   return 1 + persistence(parseFloat(res))
 }
+//The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+//What if the string is empty? Then the result should be empty object literal, {}.
+function count(string) {
+  let obj = {}
+  for (let elem of string.split("")){
+    obj[elem] = (obj[elem] || 0) + 1
+  }
+  return obj;
+}
